b093bb8fd099a06dd056109f8861d1b26042043a
Abdurahmon Majidov <109199563+abdurahmonmajidov77@users.noreply.github.com>
2023-08-20 10:24:01 +0500

    ok

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import './App.css';
+import { Canvas } from '@react-three/fiber';
+import {OrbitControls, useGLTF, Stars} from "@react-three/drei"
+
+function Model(props) {
+  const { nodes, materials } = useGLTF('/shoe.gltf')
+  return (
+    <group {...props} dispose={null}>
+      <mesh geometry={nodes.shoe.geometry} material={materials.laces} />
+      <mesh geometry={nodes.shoe_1.geometry} material={materials.mesh} />
+      <mesh geometry={nodes.shoe_2.geometry} material={materials.caps} />
+      <mesh geometry={nodes.shoe_3.geometry} material={materials.inner} />
+      <mesh geometry={nodes.shoe_4.geometry} material={materials.sole} />
+      <mesh geometry={nodes.shoe_5.geometry} material={materials.stripes} />
+      <mesh geometry={nodes.shoe_6.geometry} material={materials.band} />
+      <mesh geometry={nodes.shoe_7.geometry} material={materials.patch} />
+    </group>
+  )
+}
+function App() {
+  return (
+    <div className="App">
+      <div className="box">
+        <div className="Text">
+          <h1>3D Objects</h1>
+          <p>This object was created with Blender.You can see 360*.Everything looks good. :)</p>
+        </div>
+        <div className="Model">
+          <Canvas className="Canvas">
+            <OrbitControls />
+            <ambientLight intensity={1.5} />
+            <spotLight position={[10,15,10]} angle={0.3} />
+            <Model/>
+          </Canvas>
+        </div>
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
